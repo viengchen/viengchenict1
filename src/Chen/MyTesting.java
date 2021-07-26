6ed4db52ae82bd6ba8cd922e7354a71dcac1066f
Viengchen <viengchen168@gmail.com>
2021-07-26 18:01:33 +0700

    Add Comment

--- src/Chen/MyTesting.java
+++ src/Chen/MyTesting.java
@@ -1,4 +1,15 @@
 package Chen;
 
 public class MyTesting {
+    public static void showMessage(String message) {
+        System.out.println(message);
+    }
+    public static void showMessage(int message) {
+        System.out.println(message);
+    }
+    public static void showMessage(double message) {
+        System.out.println(message);
+    }
+
 }
+
